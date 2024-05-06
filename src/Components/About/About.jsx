488c0841577ec5410774d26fa8032a611ef6aa11
VikramVJ20 <vikramvj711@gmail.com>
2024-05-06 23:15:28 +0530

    first commit

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './About.css'
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
+
+const About = ({setPlayState}) => {
+  return (
+    <div className='about'>
+        <div className="about-left">
+            <img src={about_img} alt="" className='about-img' />
+            <img src={play_icon} alt="" className='play-icon' onClick={()=>{setPlayState(true)}} />
+        </div>
+        <div className="about-right">
+            <h3>About UNIVERSITY</h3>
+            <h2>Nurturing Tomorrow Leaders Today</h2>
+            <p>Embrak on a transformative educational journey with our university's comprehensive education programs. Our cutting-edge curriculm is designed to empower students with the knowledge, skills, and experieces needed to excel in the dynamic field of education.</p>
+            <p>With a focus on innovation, hands-on learning, and personalized mentorship, our programs prepare aspiring educators to make a meaningful impact in classrooms,schools, and communities.</p>
+            <p>Whether you aspire to become a teacher, administrator, counselor, or educational leader, our diverse range of programs offers the perfect pathway to achive your goals and unlock your full potential in shaping the future of education. </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
